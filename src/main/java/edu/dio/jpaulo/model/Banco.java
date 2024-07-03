57d92ea6500fe170cda171b6d21cef257902d5f1
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2024-07-03 13:07:39 -0300

    Utilizando o lombok

--- src/main/java/edu/dio/jpaulo/model/Banco.java
+++ src/main/java/edu/dio/jpaulo/model/Banco.java
@@ -2,25 +2,17 @@ package edu.dio.jpaulo.model;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Banco {
 
+	@Getter
+	@Setter
 	private String nome;
+
+	@Getter
+	@Setter
 	private List<Conta> contas;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public List<Conta> getContas() {
-		return contas;
-	}
-
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
 
 }
--- src/main/java/edu/dio/jpaulo/model/Conta.java
+++ src/main/java/edu/dio/jpaulo/model/Conta.java
@@ -1,13 +1,21 @@
 package edu.dio.jpaulo.model;
 
+import lombok.Getter;
+
 public abstract class Conta implements IConta {
 	
 	private static final int AGENCIA_PADRAO = 1;
 	private static int SEQUENCIAL = 1;
 
+	@Getter
 	protected int agencia;
+	
+	@Getter
 	protected int numero;
+	
+	@Getter
 	protected double saldo;
+	
 	protected Cliente cliente;
 
 	public Conta(Cliente cliente) {
@@ -30,18 +38,6 @@ public abstract class Conta implements IConta {
 	public void transferir(double valor, IConta contaDestino) {
 		this.sacar(valor);
 		contaDestino.depositar(valor);
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
 	}
 
 	protected void imprimirInfosComuns() {
