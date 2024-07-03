3850ad32d16a1dca905a798590dcfc3d9a0384a6
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2024-07-03 13:01:11 -0300

    Inclusão das classes de referência do desafio

---
+++ src/main/java/edu/dio/jpaulo/model/ContaCorrente.java
@@ -0,0 +1,16 @@
+package edu.dio.jpaulo.model;
+
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Corrente ===");
+		super.imprimirInfosComuns();
+	}
+	
+}
